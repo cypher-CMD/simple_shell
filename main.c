9b4b867d57c332f179b80475a0b6316c1ba84cb2
Godwin Enahoro <giogdxgas1@gmail.com>
2022-06-17 14:55:34 -0700

    final patches

--- main.c
+++ main.c
@@ -1,41 +1,73 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - Command-line interpreter
- * @argc: argument count
- * @argv: argument vector
+ * free_data - frees data structure
  *
- * Return: Always 0 (Success process)
- *
- * Authors: Monica Ortiz - Abdel Mejia
+ * @datash: data structure
+ * Return: no return
  */
-int main(__attribute__((unused))int argc, __attribute__((unused)) char *argv[])
+void free_data(data_shell *datash)
 {
-	char *buffer = NULL;
-	size_t buff_size = 0;
-	ssize_t line = 0;
+	unsigned int i;
 
-	if (!isatty(0))
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		line = getline(&buffer, &buff_size, stdin);
-		process(buffer);
-	}
-	else
-	{
-		do {
-			prompt();
-			line = getline(&buffer, &buff_size, stdin);
-
-			if (line == EOF)
-			{
-				free(buffer);
-				_putchar('\n');
-				return (EXIT_SUCCESS);
-			}
-
-			process(buffer);
-		} while (line != EOF);
+		free(datash->_environ[i]);
 	}
 
-	return (EXIT_SUCCESS);
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
